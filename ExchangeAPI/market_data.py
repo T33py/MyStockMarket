33c0045f2e55a03c6bd1ee4d2e53fd750aa888c6
t33py <t33py@outlook.com>
2023-10-25 22:50:03 +0200

    Started working on order execution

--- ExchangeAPI/market_data.py
+++ ExchangeAPI/market_data.py
@@ -1,7 +1,8 @@
 # data.py
-from sqlalchemy import create_engine, Column, Integer, String, Float
+from sqlalchemy import create_engine, Column, Integer, String, Float, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from consts import ordertypes, orderstatuses
 
 # Create a SQLAlchemy database engine
 DATABASE_URL = "sqlite:///exchange.db"
@@ -24,14 +25,14 @@ class Position(Base):
     __tablename__ = 'positions'
 
     id = Column(Integer, primary_key=True, index=True)
-    customer_id = Column(Integer, index=True)
-    stock_symbol = Column(String, index=True)
+    customer_id = Column(Integer, ForeignKey('customers.id'), index=True)
+    stock_symbol = Column(String, ForeignKey('stocks.symbol'), index=True)
     number_of_shares = Column(Integer)
     bought_at_price = Column(Float)
     bought_at_date = Column(String)
 
 class Stock(Base):
-    __tablename__ = 'Stocks'
+    __tablename__ = 'stocks'
     
     id = Column(Integer, primary_key=True, index=True)
     stock_symbol = Column(String, index=True)
@@ -45,11 +46,12 @@ class Order(Base):
     __tablename__ = 'orders'
 
     id = Column(Integer, primary_key=True, index=True)
-    customer_id = Column(Integer, index=True)
+    customer_id = Column(Integer, ForeignKey('customers.id'), index=True)
     order_type = Column(String)  # 'buy' or 'sell'
-    stock_symbol = Column(String)
+    stock_symbol = Column(String, ForeignKey('stocks.symbol'), index=True)
     quantity = Column(Integer)
     price = Column(Float)
+    status = Column(String)
 
 Base.metadata.create_all(bind=engine)
 
@@ -63,6 +65,105 @@ def get_all_customers()-> list[Customer]:
         session.close()
 
 
+def create_new_order(customer_id: int, order_type: String, stock_symbol: String, quantity: int, price: float)-> Order|None:
+    '''
+    Create a new order in the market
+    '''
+    session = Session()
+    new_order = Order(
+        customer=customer_id, 
+        order_type=order_type, 
+        stock_symbol=stock_symbol, 
+        quantity=quantity, price=price, 
+        status=orderstatuses.ACTIVE
+        )
+    try:
+        session.add(new_order)
+        session.commit()
+        session.refresh(new_order) # get order ID
+        return new_order
+    except:
+        new_order = None
+    finally:
+        session.close()
+    return new_order
+
+def get_orders(stock_symbol)-> list[Order]:
+    session = Session()
+    try:
+        return session.query(Order).filter(Order.stock_symbol == stock_symbol).all()
+    finally:
+        session.close()
+    return None
+
+def execute_order(order: int, counterparty: int)-> Order|None:
+    '''
+    Execute the provided order with the customer id provided as counterparty.
+    This will only execute ACTIVE orders, and returns the order if successful, otherwise None.
+    TODO: implement something to indicate why stuff failed
+    '''
+    session = Session()
+    try:
+        # is the order in the correct state?
+        order = session.query(Order).filter(Order.id).first()
+        if order is None:
+            return None
+        if order.status != orderstatuses.ACTIVE:
+            return None
+        
+        # does the buyer have the funds?
+        buyer_account = session.query(Customer).filter(Customer.id == buyer.id).first()
+        seller_account = session.query(Customer).filter(Customer.id == counterparty).first()
+        total = order.price * order.quantity
+        if buyer_account is None or seller_account is None:
+            return None
+        if buyer_account.account_balance < total:
+            return None
+        
+        # does the seller hold the position?
+        seller = None
+        buyer = None
+        create_buyer = False
+        if order.order_type == ordertypes.BUY:
+            seller = session.query(Position).filter(Position.customer_id == counterparty and Position.stock_symbol == order.stock_symbol).first()
+            buyer = session.query(Position).filter(Position.customer_id == order.customer_id).first()
+            if buyer is None:
+                buyer = Position(customer_id=order.customer_id, stock_symbol=order.stock_symbol, quantity=order.quantity, bought_at_price=order.price, bought_at_date='2000-12-31')
+                create_buyer = True
+        else: # it is a sell order
+            buyer = session.query(Position).filter(Position.customer_id == counterparty).first()
+            seller = session.query(Position).filter(Position.customer_id == order.customer_id and Position.stock_symbol == order.stock_symbol).first()
+            if buyer is None:
+                buyer = Position(customer_id=counterparty, stock_symbol=order.stock_symbol, quantity=order.quantity, bought_at_price=order.price, bought_at_date='2000-12-31')
+                create_buyer = True
+
+        if seller is None:
+            return None
+        
+        # update the positions
+        buyer_account.account_balance -= total
+        seller_account += total
+
+        if create_buyer:
+            session.add(buyer)
+        else:
+            pr_qty = buyer.number_of_shares
+            pr_avg = buyer.bought_at_price
+            buyer.number_of_shares += order.quantity
+            buyer.bought_at_price = (pr_qty * pr_avg + total)/buyer.number_of_shares
+
+        seller.number_of_shares -= order.quantity
+
+        # update the order and commit
+        order.status = orderstatuses.CLOSED
+        session.commit()
+        session.refresh(buyer)
+        return order
+    finally:
+        session.close()
+    return None
+
+
 def create_new_customer(name: String, account_balance: Float)-> Customer|None:
     '''
     Create a customer with a name that is not currently in use.
@@ -70,7 +171,9 @@ def create_new_customer(name: String, account_balance: Float)-> Customer|None:
     account_balance: initial balance of the account
     '''
     session = Session()
-    new_customer = Customer(name=name, account_balance=account_balance)
+    new_customer = Customer(
+        name=name, 
+        account_balance=account_balance)
     try:
         # Create a new customer based on the provided data
         session.add(new_customer)
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # MyStockMarket
 Hopefully this becomes a game at some point in the future
+
+
+## ExchangeAPI
+This project is the securities exchanges API that stock brokers can use to retrieve information about everything in the market.
+
+Currently it also acts as a bank, since all security holders should be registered there anyway.
